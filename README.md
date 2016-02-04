6b258f14957c93d72b8553c4fc9577d0091f683f
hckhanh <hoangchaukhanh93@gmail.com>
2016-02-04 11:16:04 +0700

    Add gulp configuration for client and server

--- README.md
+++ README.md
@@ -5,9 +5,16 @@ Follow the tutorial of React to learn React :3
 
 In this demo, I am using ES2015 syntaxs for the source code.
 
-## How to run
+## Run in DEBUG mode
 
 ```sh
 	npm install
 	npm start
+```
+
+## Run in RELEASE mode
+
+```sh
+	npm install
+	npm release
 ```
--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,10 @@ var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 var uglify = require('gulp-uglify');
 var streamify = require('gulp-streamify');
+var nodemon = require('gulp-nodemon');
+var exec = require('gulp-exec');
 
-gulp.task('build', function () {
+gulp.task('build_client', function () {
     return browserify({entries: './src/main.jsx', extensions: ['.jsx'], debug: true})
         .transform('babelify', {presets: ['es2015', 'react']})
         .bundle()
@@ -13,11 +15,20 @@ gulp.task('build', function () {
         .pipe(gulp.dest('js'));
 });
 
-gulp.task('watch', ['build'], function () {
-    gulp.watch('src/*.jsx', ['build']);
+gulp.task('watch_client', ['build_client'], function () {
+    gulp.watch('src/*.jsx', ['build_client']);
 });
 
-gulp.task('release', function () {
+gulp.task('watch_server', function() {
+    return nodemon({
+        script: 'app.js',
+        ext: 'js',
+        ignore: ['js/'],
+        env: { 'NODE_ENV': 'development' }
+    })
+});
+
+gulp.task('release_client', function() {
     return browserify({entries: './src/main.jsx', extensions: ['.jsx'], debug: true})
         .transform('babelify', {presets: ['es2015', 'react']})
         .bundle()
@@ -26,4 +37,13 @@ gulp.task('release', function () {
         .pipe(gulp.dest('js'));
 });
 
-gulp.task('default', ['watch']);
+gulp.task('release_server', function (cb) {
+  exec('node app.js', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+});
+
+gulp.task('default', ['watch_client', 'watch_server']);
+gulp.task('release', ['release_client', 'release_server']);
